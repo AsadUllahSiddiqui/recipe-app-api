e10a7a16ff9df070471fccffcf7fbb270b040982
Badar Janjua <asadSiddiquidev@gmail.com>
2023-04-11 22:30:29 +0500

    inital test setup

---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+  """ test the calc modile."""
+
+  def test_add_numbers(self):
+    """test adding numbers """
+    res = calc.add(5, 6)
+
+    self.assertEqual(res, 1 1)
+
+use unittest.mock
+patch -overrides code for tests
+MagicMock/Mock -Replace real objects
